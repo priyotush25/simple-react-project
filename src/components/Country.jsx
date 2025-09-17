db51751215030dfd51425a8423c8ee471c3f0572
Priyotush <priyotushpaul40@gmail.com>
2025-09-17 22:00:48 +0600

    conditional css add

--- src/components/Country.jsx
+++ src/components/Country.jsx
@@ -1,15 +1,21 @@
+import { useState } from "react";
+
 const Country = ({ country }) => {
+  const [visited, setVisited] = useState(false);
+
   const visitedHandle = () => {
-    console.log("visited");
+    setVisited(!visited);
   };
 
   return (
-    <div className="box">
+    <div className={`box ${visited && "visited"}`}>
       <h2>Name : {country.name.common}</h2>
       <img src={country.flags.png} alt="" />
       <h4>Independent : {country.population}</h4>
 
-      <button onClick={visitedHandle}>Visited</button>
+      <button onClick={visitedHandle}>
+        {visited ? "Visited" : "Not Visited"}
+      </button>
     </div>
   );
 };
